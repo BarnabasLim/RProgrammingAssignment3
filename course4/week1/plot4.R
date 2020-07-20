15f05168ed93fe9e167e38a790c0bae86d56d05d
Barnabas Lim <blim048@e.ntu.edu.sg>
2020-07-20 23:09:46 +0800

    Rectified Legend

--- course4/week1/plot4.R
+++ course4/week1/plot4.R
@@ -27,6 +27,7 @@ unique(h_con_data$Date)
 h_con_data<-h_con_data[!is.na(h_con_data$Time),]
 
 #plot 4
+png(filename = "./course4/week1/plot4.png")
 par(mfrow=c(2,2))
 with(h_con_data,plot(DateTime,Global_active_power,type="l",ylab = "Global Active Power (kilowatts)",xlab = ""))
 with(h_con_data,plot(DateTime,Voltage,type="l",ylab = "Voltage"))
@@ -36,5 +37,4 @@ with(h_con_data,{plot(DateTime,Sub_metering_1,ylab = "Energy sub metering",type=
   legend("topright", lty=1, cex=0.8,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))})
 with(h_con_data,plot(DateTime,Global_reactive_power,type="l",ylab = "Global_reactive_power"))
 
-dev.copy(png,file="./course4/week1/plot4.png")
 dev.off()
